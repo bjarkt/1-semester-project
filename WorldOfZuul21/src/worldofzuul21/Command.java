1608efd7cbfd2b4962d0a678fc6777ad703f9844
Kristian <>
2017-10-06 14:29:50 +0200

    Har kommenteret på koden under klassen Command

--- WorldOfZuul21/src/worldofzuul21/Command.java
+++ WorldOfZuul21/src/worldofzuul21/Command.java
@@ -6,12 +6,12 @@ package worldofzuul21;
  */
 public class Command {
 
-    /*Defining attrubitese commandWord of the datatype CommandWord, and secondWord 
+    /* Defining attrubitese commandWord of the datatype CommandWord, and secondWord 
 of the datatype String  */
     private CommandWord commandWord;
     private String secondWord;
 
-    /*Creating Constructor for the class Command. The constructer has to parameters,
+    /* Creating Constructor for the class Command. The constructer has to parameters,
     which takes arguments of the datatype CommandWord and String.
     The parameters is set equal to the attributes of the class.
      */
@@ -19,24 +19,24 @@ of the datatype String  */
         this.commandWord = commandWord;
         this.secondWord = secondWord;
     }
-//Method that enables the aqquirement of the attribute commandWord.
+//  Method that enables the aqquirement of the attribute commandWord.
 
     public CommandWord getCommandWord() {
         return commandWord;
     }
-//Method that enables the aqquirement of the attribute secondWord.
+//  Method that enables the aqquirement of the attribute secondWord.
 
     public String getSecondWord() {
         return secondWord;
     }
 
-    /* Boolean Method that checks if the attribute commandWord is equal to that of 
+    /*Boolean Method that checks if the attribute commandWord is equal to that of 
     CommandWord.UNNKOWN.
      */
     public boolean isUnknown() {
         return (commandWord == CommandWord.UNKNOWN);
     }
-//  Method that checks the boolen expression: secondWord != null, 
+//   Method that checks the boolen expression: secondWord != null, 
 
     public boolean hasSecondWord() {
         return (secondWord != null);
