c75296010f3ccd522400bf0d0db44e066d9ba80b
bjtob <bjarke.tobiesen@gmail.com>
2017-10-06 14:45:08 +0200

    Skrev kommentar til halvdelen af Room klassen.

--- WorldOfZuul21/src/worldofzuul21/Room.java
+++ WorldOfZuul21/src/worldofzuul21/Room.java
@@ -30,21 +30,28 @@ public class Room
         return description;
     }
 
+    /* Returns the description of the room, and exits for the room. */
     public String getLongDescription()
     {
         return "You are " + description + ".\n" + getExitString();
     }
 
-	private String getExitString()
+    /* Returns all the exits for the room as a string. */
+    private String getExitString()
     {
         String returnString = "Exits:";
+        // Get the keys from the exits map (the strings).
         Set<String> keys = exits.keySet();
         for(String exit : keys) {
+            // For each string, called exit, in the Set of keys, 
+            // append it to the string.
             returnString += " " + exit;
         }
         return returnString;
     }
-
+    
+    /* Returns room, that has the corresponding direction. 
+       Returns null if there is no room, for a certain direction. */
     public Room getExit(String direction) 
     {
         return exits.get(direction);
