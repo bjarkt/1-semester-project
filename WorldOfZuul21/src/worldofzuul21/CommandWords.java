f92e7538ed21c1fa71fe121cb1a3e3650471fd82
Kristian <>
2017-10-09 11:53:38 +0200

    Alt shift f

--- WorldOfZuul21/src/worldofzuul21/CommandWords.java
+++ WorldOfZuul21/src/worldofzuul21/CommandWords.java
@@ -3,43 +3,38 @@ package worldofzuul21;
 import java.util.HashMap;
 
 /**
- * @author  Michael Kolling and David J. Barnes
+ * @author Michael Kolling and David J. Barnes
  * @version 2006.03.30
  */
+public class CommandWords {
 
-public class CommandWords
-{
     private HashMap<String, CommandWord> validCommands; //A hashmap with the constants from the enom class CommandWords as values.
-    
+
     public CommandWords() // Constructor
     {
         validCommands = new HashMap<String, CommandWord>(); // Creating an object
-        for(CommandWord command : CommandWord.values()) { //For each loop - runs 4 times as CommandWord.values has 4 values.
-            if(command != CommandWord.UNKNOWN) {  //Checks if command isn't UNKNOWN ("?")
+        for (CommandWord command : CommandWord.values()) { //For each loop - runs 4 times as CommandWord.values has 4 values.
+            if (command != CommandWord.UNKNOWN) {  //Checks if command isn't UNKNOWN ("?")
                 validCommands.put(command.toString(), command); //If it isn't, a new entry is added to the validCommands HashMap
             }
         }
     }
 
-    public CommandWord getCommandWord(String commandWord)
-    {
+    public CommandWord getCommandWord(String commandWord) {
         CommandWord command = validCommands.get(commandWord);
-        if(command != null) { //Checks if command isnt null, so it can be returned, else UNKNOWN ("?") is returned
+        if (command != null) { //Checks if command isnt null, so it can be returned, else UNKNOWN ("?") is returned
             return command;
-        }
-        else {
+        } else {
             return CommandWord.UNKNOWN;
         }
     }
-    
-    public boolean isCommand(String aString)
-    {
+
+    public boolean isCommand(String aString) {
         return validCommands.containsKey(aString); //Returns true if the HashMap contains an entry for the key
     }
 
-    public void showAll() 
-    {
-        for(String command : validCommands.keySet()) { //For each loop, that prints out every key in the HashMap validCommands.
+    public void showAll() {
+        for (String command : validCommands.keySet()) { //For each loop, that prints out every key in the HashMap validCommands.
             System.out.print(command + "  ");
         }
         System.out.println();
