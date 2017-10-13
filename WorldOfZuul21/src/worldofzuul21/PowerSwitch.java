ca91b4d405dac0fa0e2ac8a4ea579864d1585a2f
Kristian <>
2017-10-13 12:14:21 +0200

    PowerSwitch klassen

---
+++ WorldOfZuul21/src/worldofzuul21/PowerSwitch.java
@@ -0,0 +1,47 @@
+
+package worldofzuul21;
+
+public class PowerSwitch {
+
+boolean status;
+
+    PowerSwitch() {
+    }
+
+    void turnPowerOff() {
+        status = false;
+
+    }
+
+    void turnPowerOn() {
+        status = true;
+
+    }
+
+
+}
+
+/* Så hvis man tilføjer en attribut til Room: Powerswtich powerswitch = new Powerswitch(); 
+,så kan man lave en switch i game metoden.
+int number = (int)(Math.random() * 3); 
+
+switch (number){
+    case 1;
+       room02.powerswtich.turnPowerOn
+    case 2;
+       room04.powerswtich.turnPowerOn 
+    case 3;
+       room11.powerswtich.turnPowerOn
+}
+
+Så skal vi bare have lavet en måde hvorpå vi kan interact. Det gøres nok ved at 
+tjekke statement. if(currentroom.powerswitch.status) {
+    Turnoflights(); (Det er så metoden fra interact)
+} else {
+    break;
+}
+
+
+
+    
+}
