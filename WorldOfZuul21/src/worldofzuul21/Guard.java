ba7ba7e1a36b6cb68ad1ff66d7998a66f60fa6b8
GGkarma <jepper01@hotmail.com>
2017-10-27 13:11:49 +0200

    Comments for guard and loacation

--- WorldOfZuul21/src/worldofzuul21/Guard.java
+++ WorldOfZuul21/src/worldofzuul21/Guard.java
@@ -15,18 +15,22 @@ public class Guard {
     private Room currentRoom;
     
     Guard(int ID) {
+        // ID for Guard objekt
         this.ID = ID;
     }
     
     public int getID() {
+        // metohod for getting ID
         return ID;
     }
     
     public Room getRoom() {
+        //metodhod for getting currentRoom
         return currentRoom;
     }
     
     public void setRoom(Room room) {
+        // set metodhod for room
         currentRoom = room;
     }
 }
