f55a4ab2b8f3800efc5061ab4fdcdffbcf242f64
Oversaftigt <32249246+Oversaftigt@users.noreply.github.com>
2017-10-27 13:02:10 +0200

    Kommentering a "Item"
    Martin og Thomas

--- WorldOfZuul21/src/worldofzuul21/Item.java
+++ WorldOfZuul21/src/worldofzuul21/Item.java
@@ -5,22 +5,28 @@ import java.util.*;
 public class Item {
 
     private String name;
-
+    //Naming the possible items which can be stolen
     private static String[] itemNames = {"Painting", "Bust", "Jewel", "Vase", "Diamond"};
+    //Items which have been stolen are added to this arraylist, so another item can be stolen
     private static List<String> usedItems = new ArrayList<>();
-
+    
+    
     public Item(String name) {
         this.name = name;
     }
-
+    
+    //Getter method for "name"
     public String getName() {
         return name;
     }
-
+    //Setter method for "name"
     public void setName(String name) {
         this.name = name;
     }
-
+    
+    /* A loop where items can spawn around. If an item has been spawned before,
+    it is added to usedItems, so it cannot be spawned agian  
+    */
     public static String spawnItem(List<Room> rooms) {
         for (Room room : rooms) {
             room.setItem(null);
