28bb0cfb49f4a9fc7a70092461e3cba63f2ab298
Kristian <Kristian@DESKTOP-UHPFBGI>
2017-11-02 08:36:56 +0100

    Tekst to PowerSwitch

--- WorldOfZuul21/src/worldofzuul21/PowerSwitch.java
+++ WorldOfZuul21/src/worldofzuul21/PowerSwitch.java
@@ -4,49 +4,23 @@ package worldofzuul21;
 public class PowerSwitch {
 
     private boolean isOn;
-
+/* No arg constructer */
     PowerSwitch() {
     }
-
+/* Method to set IsOn to false */
     void turnPowerOff() {
         isOn = false;
 
     }
-
+/* Method to set IsOn to true */
     void turnPowerOn() {
         isOn = true;
 
     }
-
+/* Method to return the state of IsON */
     public boolean getIsOn() {
         return isOn;
     }
 
 
 }
-
-/* Så hvis man tilføjer en attribut til Room: Powerswtich powerswitch = new Powerswitch(); 
-,så kan man lave en switch i game metoden.
-int number = (int)(Math.random() * 3); 
-
-switch (number){
-    case 1;
-       room02.powerswtich.turnPowerOn
-    case 2;
-       room04.powerswtich.turnPowerOn 
-    case 3;
-       room11.powerswtich.turnPowerOn
-}
-
-Så skal vi bare have lavet en måde hvorpå vi kan interact. Det gøres nok ved at 
-tjekke statement. if(currentroom.powerswitch.status) {
-    Turnoflights(); (Det er så metoden fra interact)
-} else {
-    break;
-}
-
-
-
-    
-}
-*/
