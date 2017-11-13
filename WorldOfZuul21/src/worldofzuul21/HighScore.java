8f89faf5e9797950f2982600789d9559e8d4cb8c
Unknown <bjarke.tobiesen@gmail.com>
2017-11-13 16:33:28 +0100

    private variabler og compareTo

--- WorldOfZuul21/src/worldofzuul21/HighScore.java
+++ WorldOfZuul21/src/worldofzuul21/HighScore.java
@@ -1,8 +1,8 @@
 package worldofzuul21;
 
 public class HighScore implements Comparable<HighScore> {
-    String name;
-    int score;
+    private String name;
+    private int score;
 
     public HighScore(String name, int score) {
         this.name = name;
@@ -16,12 +16,6 @@ public class HighScore implements Comparable<HighScore> {
 
     @Override
     public int compareTo(HighScore other) {
-        if (this.score > other.score) {
-            return 1;
-        } else if (this.score < other.score) {
-            return -1;
-        } else {
-            return 0;
-        }
+        return this.score - other.score;
     }
 }
