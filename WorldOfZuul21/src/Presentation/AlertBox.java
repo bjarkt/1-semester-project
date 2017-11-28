feebb123805cff1747fcf22fea196282d58f5911
Unknown <bjarke.tobiesen@gmail.com>
2017-11-28 22:47:13 +0100

    gør fil hentning mere platformsuafhængigt
    virker i intellij & netbeans på windows og intellij på linux

--- WorldOfZuul21/src/Presentation/AlertBox.java
+++ WorldOfZuul21/src/Presentation/AlertBox.java
@@ -4,6 +4,9 @@ package Presentation;
 import Acq.IHighScore;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -18,24 +21,33 @@ import javafx.stage.*;
 
 public class AlertBox {
 
-    public static void display(String header, String textfilename) {
-        String text = "";
+    static void display(String header, String textfilename) {
+        StringBuilder text = new StringBuilder();
         Stage window = new Stage();
         window.setTitle(header);
         window.setMinWidth(400);
         window.setMaxWidth(600);
-        Scanner input = null;
-        File file = new File(textfilename);
+        Scanner input;
+
+        File directory = new File("");
+        String realFilePath;
+        if (directory.getAbsolutePath().contains("WorldOfZuul21")) {
+            realFilePath = directory.getAbsolutePath() + "/src/Presentation/Textfiles/" + textfilename;
+        } else {
+            realFilePath = directory.getAbsolutePath() + "/WorldOfZuul21/src/Presentation/Textfiles/" + textfilename;
+        }
+        File file = new File(realFilePath);
+
         try {
             input = new Scanner(file);
+            while (input.hasNext()) {
+                text.append(input.nextLine()).append(System.lineSeparator());
+            }
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(AlertBox.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        while (input.hasNext()) {
-            text += input.nextLine() + System.lineSeparator() ;
+            System.err.println("Error: File at " + realFilePath + " not found.");
         }
         Label label = new Label();
-        label.setText(text);
+        label.setText(text.toString());
         label.setWrapText(true);
 
         Button closeButton = new Button("Close");
@@ -49,8 +61,7 @@ public class AlertBox {
         window.setScene(scene);
         window.showAndWait();
     }
-    public static void display(String header, List<IHighScore> highScore ) {
-        String text = "";
+    static void display(String header, List<IHighScore> highScore ) {
         Stage window = new Stage();
         window.setTitle(header);
         window.setMinWidth(400);
